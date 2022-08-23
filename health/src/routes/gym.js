9242338c756df5d02567979832cc3ac5ec2ac922
Ko hyeon lim <95522176+indianaPoly@users.noreply.github.com>
2022-08-23 14:59:13 +0900

    헬스장 위치 가져오기 수정 중

--- health/src/routes/gym.js
+++ health/src/routes/gym.js
@@ -1,62 +1,64 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
 import { Button, ButtonGroup, DropdownButton, Dropdown } from "react-bootstrap";
-
-// 서버에서 데이터를 가지고 오기 전에 array를 비웠으면 좋겠음.
-// 그 때 배운거 쓰면 될 듯?
+import { useQuery } from '@tanstack/react-query';
+import { useState } from "react";
 
 function Gym() {
 
-  let nameArray,
-    loactionArray = [];
-  let [name, setName] = useState(nameArray);
-  let [location, setLocation] = useState(loactionArray);
-  let [gudata, setGudata] = useState(null);
-  var i, j;
-
-  useEffect(() => {
-    setName([]);
-  },[gudata]);
-  // 버튼을 누르면 값을 바로 가져와야 하는데 값을 가져오고 array를 초기화 함.
-  // 처음에는 무조건 빈 배열을 넣는건가..?
-
-  const onClick = async () => {
-    try {
-      const response = await axios.get(
-        `http://openapi.seoul.go.kr:8088/${process.env.REACT_APP_GYM_API_KEY}/json/LOCALDATA_104201/1/1000/`
-      );
-      // 추후에는 response.data.LOCALDATA_104201["list_total_count"] 이 값을 반복문에 넣을예정
-      for (i = 0; i < 1000; i++) {
-        // 영업중인 헬스장을 1차로 분류함.
-        let opengymNameData =
-          response.data.LOCALDATA_104201["row"][i].DTLSTATENM === "영업중"
-            ? response.data.LOCALDATA_104201["row"][i]
-            : null;
-
-        // 여기서 데이터를 처리하기 위해서는 useEffect를 제대로 이해하고 넘아가야 할 것 같음.
-        // for (j = 0 ; j < 1000 ; j++) {
-        //   let gymNameData = (opengymNameData[j].RDNWHLADDR).indexOf(gudata) === gudata ? response.data.LOCALDATA_104201["row"][i] : null;
-        //   setName((nameArray) => [...nameArray, gymNameData]);
-        // }
-       setName((nameArray) => [...nameArray, opengymNameData]);
-        // setLocation((loactionArray) => [...loactionArray, gymLocationData]);
-      }
-      console.log(name);
-      console.log(name[3].RDNWHLADDR);
-    } catch (e) {
-      console.log(e);
-    }
-  };
-
   return (
     <>
-      <SelectedLocation onClick={onClick} setGudata={setGudata}></SelectedLocation>
-      {/* <SelectedGU></SelectedGU> */}
+      <SelectedLocation></SelectedLocation>
     </>
   );
 }
 
-function SelectedLocation(props) {
+function SelectedLocation() {
+
+  const result = useQuery(
+    ["todos"],
+    async () =>
+      await axios
+        .get(
+          `http://openapi.seoul.go.kr:8088/${process.env.REACT_APP_GYM_API_KEY}/json/LOCALDATA_104201/1/1000/`
+        )
+        .then((a) => {
+          return a.data;
+        })
+  );
+
+  const [location, setLocation] = useState(null);
+
+  let onoffArray = [];
+  let guonoffArray = [];
+  const [onoff, setOnoff] = useState(onoffArray);
+  const [guonoff, setGuOnoff] = useState(guonoffArray);
+
+  const handleSelect = (e) => {
+    setLocation(e);
+    var i, j;
+    /**
+     * @memo 서울시에서 가져온 공공데이터 중에서 영업중인 곳에 대한 데이터를 openGymData에 저장하고 이를 Onoff 배열에 밀어넣는 식으로 저장
+     */
+    for (i = 0 ; i < 1000 ; i++){
+      if (result.data.LOCALDATA_104201.row[i]["DTLSTATENM"] === "영업중"){
+        let openGymData = result.data.LOCALDATA_104201.row[i];
+        setOnoff((onoffArray) => [...onoffArray, openGymData]);
+      }
+    }
+    console.log(onoff);
+    /**
+     * @memo 영업중인 헬스장 위치가 선택된 지역(e)이면 또 다른 배열에 저장 
+     */
+    // for (j = 0 ; j < 1000; j++){
+    //   // 영업중인 업소중(onoff[i])에서 현재 선택한 지역(e)이 있으면 추가
+    //   if ( (onoff[j]["RDNWHLADDR"]).indexOf(e) != -1){
+    //     let GuOpenGymData = result.data.LOCALDATA_104201.row[j];
+    //     setGuOnoff((guonoffArray) => [...guonoffArray, GuOpenGymData]);
+    //   }
+    // }
+    // console.log(guonoff);
+  }
+
   return (
     <>
       <ButtonGroup
@@ -64,14 +66,15 @@ function SelectedLocation(props) {
           marginTop: "30px",
         }}
       >
-        <Button onClick={() => props.onClick()}>서울에 살아요.</Button>
+        <Button>서울에 살아요.</Button>
         {/* <Button>2</Button> */}
 
+        {/* 각각의 버튼이 눌렀을 경우에 배열을 초기화를 시켜야 함. */}
         <DropdownButton
           as={ButtonGroup}
           title="사는 동네를 설정해주세요."
           id="bg-nested-dropdown"
-          onSelect={(eventKey) => {props.setGudata(eventKey)}}
+          onSelect={handleSelect}
         >
           <Dropdown.Item eventKey="강남구">강남구</Dropdown.Item>
           <Dropdown.Item eventKey="강동구">강동구</Dropdown.Item>
@@ -100,17 +103,13 @@ function SelectedLocation(props) {
           <Dropdown.Item eventKey="중량구">중량구</Dropdown.Item>
         </DropdownButton>
       </ButtonGroup>
+
+      <div> {result.data && result.data.LOCALDATA_104201.row[4]["DTLSTATENM"]} </div>
+      <div> {result.data && result.data.LOCALDATA_104201.row[4]["RDNWHLADDR"]} </div>
+      <div> {location} </div>
     </>
   );
 }
-
-// function SelectedGU(eventKey){
-//   return(
-//   <>
-//   <div> {eventKey}에 있는 헬스장을 보여드릴께요.</div>
-//   </>);
-// }
-
 export default Gym;
 
 // 헬스장 정보에 관해서 현재 위치 가져오기
