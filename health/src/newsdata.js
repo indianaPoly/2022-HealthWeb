819ed6829ba57485e920a026dfb803258251a88a
Ko hyeon lim <95522176+indianaPoly@users.noreply.github.com>
2022-08-29 21:09:33 +0900

    Update newsdata.js

--- health/src/newsdata.js
+++ health/src/newsdata.js
@@ -1,104 +1,108 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import { Carousel } from "react-bootstrap";
 import { useQuery } from "@tanstack/react-query";
 
 function NewsData() {
+  return <ControlledCarousel></ControlledCarousel>;
+}
+
+function ControlledCarousel() {
   
-  const result = useQuery(
-    ["todos"],
-    async () =>
-      await axios
-        .get(
-          `https://newsapi.org/v2/everything?q=Health&from=2022-08-01&to=2022-08-26&sortBy=popularity&apiKey=${process.env.REACT_APP_newsDataAPIKEY}`
-        )
-        .then((a) => {
-          return a.data;
-        })
+  let result = useQuery(["todos"], () =>
+    axios
+      .get(
+        `https://newsapi.org/v2/everything?q=Health&from=2022-08-01&to=2022-08-26&sortBy=popularity&apiKey=${process.env.REACT_APP_newsDataAPIKEY}`
+      )
+      .then((a) => {
+        return a.data;
+      })
   );
+
+  console.log(result.data);
 
   return (
     <div>
-      <ControlledCarousel result={result}></ControlledCarousel>
+      {result.isLoading && "로딩중입니다. 잠시만 기다려주세요."}
+      {result.error && "데이터 불러오기가 실패하였습니다."}
+      {result.data && (
+        <div>
+          <Carousel>
+            <Carousel.Item>
+              <a href={result.data.articles[1]["url"]}>
+                <img
+                  className="d-block w-100 "
+                  src={result.data.articles[1]["urlToImage"]}
+                  alt="First slide"
+                />
+                <Carousel.Caption>
+                  <h3
+                    style={{
+                      color: "#fff",
+                      letterSpacing: "-1px",
+                      fontSize: "23px",
+                      fontWeight: 700,
+                      textShadow: "1px 1px 3px #222",
+                    }}
+                  >
+                    {" "}
+                    {result.data.articles[1]["title"]}{" "}
+                  </h3>
+                </Carousel.Caption>
+              </a>
+            </Carousel.Item>
+
+            <Carousel.Item>
+              <a href={result.data.articles[2]["url"]}>
+                <img
+                  className="d-block w-100"
+                  src={result.data.articles[2]["urlToImage"]}
+                  alt="First slide"
+                />
+                <Carousel.Caption>
+                  <h3
+                    style={{
+                      color: "#fff",
+                      letterSpacing: "-1px",
+                      fontSize: "23px",
+                      fontWeight: 700,
+                      textShadow: "1px 1px 3px #222",
+                    }}
+                  >
+                    {" "}
+                    {result.data.articles[2]["title"]}{" "}
+                  </h3>
+                </Carousel.Caption>
+              </a>
+            </Carousel.Item>
+
+            <Carousel.Item>
+              <a href={result.data.articles[3]["url"]}>
+                <img
+                  className="d-block w-100"
+                  src={result.data.articles[3]["urlToImage"]}
+                  alt="First slide"
+                />
+                <Carousel.Caption>
+                  <h3
+                    style={{
+                      color: "#fff",
+                      letterSpacing: "-1px",
+                      fontSize: "23px",
+                      fontWeight: 700,
+                      textShadow: "1px 1px 3px #222",
+                    }}
+                  >
+                    {" "}
+                    {result.data.articles[3]["title"]}{" "}
+                  </h3>
+                </Carousel.Caption>
+              </a>
+            </Carousel.Item>
+          </Carousel>
+        </div>
+      )}
     </div>
-  );
-}
-
-function ControlledCarousel(props) {
-  return (
-    <Carousel>
-      <Carousel.Item>
-        <a href={props.result.data.articles[1]["url"]}>
-          <img
-            className="d-block w-100 "
-            src={props.result.data.articles[1]["urlToImage"]}
-            alt="First slide"
-          />
-          <Carousel.Caption>
-            <h3
-              style={{
-                color: "#fff",
-                letterSpacing: "-1px",
-                fontSize: "23px",
-                fontWeight: 700,
-                textShadow: "1px 1px 3px #222",
-              }}
-            >
-              {" "}
-              {props.result.data.articles[1]["title"]}{" "}
-            </h3>
-          </Carousel.Caption>
-        </a>
-      </Carousel.Item>
-
-      <Carousel.Item>
-        <a href={props.result.data.articles[2]["url"]}>
-          <img
-            className="d-block w-100"
-            src={props.result.data.articles[2]["urlToImage"]}
-            alt="First slide"
-          />
-          <Carousel.Caption>
-            <h3
-              style={{
-                color: "#fff",
-                letterSpacing: "-1px",
-                fontSize: "23px",
-                fontWeight: 700,
-                textShadow: "1px 1px 3px #222",
-              }}
-            >
-              {" "}
-              {props.result.data.articles[2]["title"]}{" "}
-            </h3>
-          </Carousel.Caption>
-        </a>
-      </Carousel.Item>
-
-      <Carousel.Item>
-        <a href={props.result.data.articles[3]["url"]}>
-          <img
-            className="d-block w-100"
-            src={props.result.data.articles[3]["urlToImage"]}
-            alt="First slide"
-          />
-          <Carousel.Caption>
-            <h3
-              style={{
-                color: "#fff",
-                letterSpacing: "-1px",
-                fontSize: "23px",
-                fontWeight: 700,
-                textShadow: "1px 1px 3px #222",
-              }}
-            >
-              {" "}
-              {props.result.data.articles[3]["title"]}{" "}
-            </h3>
-          </Carousel.Caption>
-        </a>
-      </Carousel.Item>
-    </Carousel>
   );
 }
 export default NewsData;
